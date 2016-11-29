afa8f9888daaebad0bb94003597a0150d65ffcce
Jon <jmcreasman89@gmail.com>
2016-11-29 14:29:01 -0500

    setting up product list template and components

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
-    </div>
+        <pm-products></pm-products>
+    </div> 
     `
 })  
 export class AppComponent {
